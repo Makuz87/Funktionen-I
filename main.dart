824d9afb8839fa72b74b5bc84809d44aff46033b
Markus Willmann <markuswillmann87@gmail.com>
2025-03-12 11:59:29 +0100

    Aufgaben erstellt

---
+++ main.dart
@@ -0,0 +1,35 @@
+void main() {
+  //Aufgabe 1
+  printNumber(4);
+  printNumber(2025);
+  printNumber(298745896321);
+
+//Aufgabe 2
+  tripplePrint("Lorem Ipsum, " * 3);
+
+// Aufgabe 3
+  String firstName = "Johnny";
+  String lastName = "Depp";
+
+  // String firstName1 = "Tom";
+  // String lastName1 = "Cruise";
+}
+
+//Aufgabe 1
+
+void printNumber(int x) {
+  print(x);
+}
+
+//Aufgabe 2
+
+void tripplePrint(String y) {
+  print(y);
+}
+
+//Aufgabe 3
+
+void reverseInitials(String first, String last) {
+  print(first[first.length]);
+  print(last[first.length]);
+}
